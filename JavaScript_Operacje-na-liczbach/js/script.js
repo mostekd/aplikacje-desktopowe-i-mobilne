e749367beb54521384b6a42b16d1bca996be4da4
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-02-26 18:47:42 +0100

    Add files via upload

--- JavaScript_Operacje-na-liczbach/js/script.js
+++ JavaScript_Operacje-na-liczbach/js/script.js
@@ -1,80 +1,105 @@
-// Funkcja pomocnicza do pobierania liczby od użytkownika
-function getNumberFromUser(promptMessage) {
-    return parseFloat(prompt(promptMessage));
-  }
+// Wprowadzenie danych przez użytkownika
+const liczba1 = parseFloat(prompt("Wprowadź pierwszą liczbę:"));
+const liczba2 = parseFloat(prompt("Wprowadź drugą liczbę:"));
+const liczba3 = parseFloat(prompt("Wprowadź trzecią liczbę:"));
 
-  // Funkcja pomocnicza do wyświetlania wyników na stronie
-  function displayResult(resultMessage, result) {
-    document.write("<p>" + resultMessage + ": " + result + "</p>");
-  }
+// Obliczenia dla pierwszej liczby
+const przeciwnyZnak1 = -liczba1;
+const suma151 = liczba1 + 15;
+const suma051 = liczba1 + 0.5;
+const kwadrat1 = liczba1 ** 2;
+const podzielonaPrzez21 = liczba1 / 2;
+const resztaZDzieleniaPrzez21 = liczba1 % 2;
+const wiekszaO11 = liczba1 + 1;
+const mniejszaO11 = liczba1 - 1;
+const zaokraglonaWdol1 = Math.floor(liczba1 / 3);
+const zaokraglonaWgore1 = Math.ceil(liczba1 / 3);
+const zaokraglona1 = Math.round(liczba1 / 3);
+const pierwiastek1 = Math.sqrt(liczba1);
+const losowaLiczba1 = Math.random() * 50;
+const iloczyn1 = liczba1 * losowaLiczba1;
+const wartoscAbsolutna1 = Math.abs(liczba1);
+const podwojonaWartosc1 = liczba1 * 2;
 
-  // Zadania
-  var number = getNumberFromUser("Wprowadź liczbę:");
+// Obliczenia dla drugiej liczby
+const przeciwnyZnak2 = -liczba2;
+const suma152 = liczba2 + 15;
+const suma052 = liczba2 + 0.5;
+const kwadrat2 = liczba2 ** 2;
+const podzielonaPrzez22 = liczba2 / 2;
+const resztaZDzieleniaPrzez22 = liczba2 % 2;
+const wiekszaO12 = liczba2 + 1;
+const mniejszaO12 = liczba2 - 1;
+const zaokraglonaWdol2 = Math.floor(liczba2 / 3);
+const zaokraglonaWgore2 = Math.ceil(liczba2 / 3);
+const zaokraglona2 = Math.round(liczba2 / 3);
+const pierwiastek2 = Math.sqrt(liczba2);
+const losowaLiczba2 = Math.random() * 50;
+const iloczyn2 = liczba2 * losowaLiczba2;
+const wartoscAbsolutna2 = Math.abs(liczba2);
+const podwojonaWartosc2 = liczba2 * 2;
 
-  var oppositeNumber = -number;
-  displayResult("Liczba o przeciwnym znaku", oppositeNumber);
+// Obliczenia dla trzeciej liczby
+const pierwiastekSumy = Math.sqrt(liczba3 + liczba3);
+const roznicaKwadratow3 = Math.pow(liczba1, 2) - Math.pow(liczba2, 2);
+const potegaTrzecia3 = Math.pow(liczba3, 3);
+const kwadrat3 = Math.pow(liczba3, 2);
+const pierwiastekSzescienny3 = Math.pow(liczba3, 1/3);
+const sredniaArytmetyczna3 = (liczba1 + liczba2) / 2;
+const pomnozPrzez103 = liczba3 * 10;
+let suma3 = 0;
+for (let i = 1; i <= liczba3; i++) {
+    suma3 += i;
+}
 
-  var sumWith15 = number + 15;
-  displayResult("Suma z 15", sumWith15);
-
-  var sumWith0_5 = number + 0.5;
-  displayResult("Suma z 0.5", sumWith0_5);
-
-  var square = Math.pow(number, 2);
-  displayResult("Kwadrat liczby", square);
-
-  var dividedBy2 = number / 2;
-  displayResult("Wartość podzielona przez 2", dividedBy2);
-
-  var remainderBy2 = number % 2;
-  displayResult("Reszta z dzielenia przez 2", remainderBy2);
-
-  var numberPlusOne = number + 1;
-  displayResult("Liczba o jeden większa", numberPlusOne);
-
-  var numberMinusOne = number - 1;
-  displayResult("Liczba o jeden mniejsza", numberMinusOne);
-
-  var roundedDownBy3 = Math.floor(number / 3);
-  displayResult("Zaokrąglona w dół po podzieleniu przez 3", roundedDownBy3);
-
-  var roundedUpBy3 = Math.ceil(number / 3);
-  displayResult("Zaokrąglona w górę po podzieleniu przez 3", roundedUpBy3);
-
-  var roundedToNearestBy3 = Math.round(number / 3);
-  displayResult("Zaokrąglona do najbliższej całkowitej po podzieleniu przez 3", roundedToNearestBy3);
-
-  var squareRoot = Math.sqrt(number);
-  displayResult("Pierwiastek liczby", squareRoot);
-
-  var randomNumber = Math.random() * 50;
-  var productWithRandom = number * randomNumber;
-  displayResult("Iloczyn z losową liczbą z przedziału od 0 do 50", productWithRandom);
-
-  var absoluteValue = Math.abs(number);
-  displayResult("Wartość absolutna liczby", absoluteValue);
-
-  var doubledValue = number * 2;
-  displayResult("Podwojona wartość liczby", doubledValue);
-
-  var secondNumber = getNumberFromUser("Wprowadź drugą liczbę:");
-  var sumOfSquares = Math.pow(number, 2) - Math.pow(secondNumber, 2);
-  displayResult("Różnica kwadratów", sumOfSquares);
-
-  var cube = Math.pow(number, 3);
-  displayResult("Potęga trzecia", cube);
-
-  var squareValue = Math.pow(number, 2);
-  displayResult("Kwadrat liczby", squareValue);
-
-  var cubeRoot = Math.cbrt(number);
-  displayResult("Pierwiastek sześcienny", cubeRoot);
-
-  var average = (number + secondNumber) / 2;
-  displayResult("Średnia arytmetyczna", average);
-
-  var multipliedByTen = number * 10;
-  displayResult("Liczba pomnożona przez 10", multipliedByTen);
-
-  var sumOfIntegers = (number * (number + 1)) / 2;
-  displayResult("Suma liczb całkowitych od 1 do wprowadzonej liczby", sumOfIntegers);
+// Wyświetlenie wyników na stronie
+const wynikiDiv = document.getElementById("wyniki");
+wynikiDiv.innerHTML = `
+  <p>Dla pierwszej liczby:</p>
+  <ul>
+    <li>Przeciwny znak: ${przeciwnyZnak1}</li>
+    <li>Suma z 15: ${suma151}</li>
+    <li>Suma z 0.5: ${suma051}</li>
+    <li>Kwadrat: ${kwadrat1}</li>
+    <li>Podzielona przez 2: ${podzielonaPrzez21}</li>
+    <li>Reszta z dzielenia przez 2: ${resztaZDzieleniaPrzez21}</li>
+    <li>Większa o 1: ${wiekszaO11}</li>
+    <li>Mniejsza o 1: ${mniejszaO11}</li>
+    <li>Zaokrąglona w dół: ${zaokraglonaWdol1}</li>
+    <li>Zaokrąglona w górę: ${zaokraglonaWgore1}</li>
+    <li>Zaokrąglona: ${zaokraglona1}</li>
+    <li>Pierwiastek: ${pierwiastek1}</li>
+    <li>Iloczyn z losową liczbą: ${iloczyn1}</li>
+    <li>Wartość absolutna: ${wartoscAbsolutna1}</li>
+    <li>Podwojona wartość: ${podwojonaWartosc1}</li>
+  </ul>
+  <p>Dla drugiej liczby:</p>
+  <ul>
+    <li>Przeciwny znak: ${przeciwnyZnak2}</li>
+    <li>Suma z 15: ${suma152}</li>
+    <li>Suma z 0.5: ${suma052}</li>
+    <li>Kwadrat: ${kwadrat2}</li>
+    <li>Podzielona przez 2: ${podzielonaPrzez22}</li>
+    <li>Reszta z dzielenia przez 2: ${resztaZDzieleniaPrzez22}</li>
+    <li>Większa o 1: ${wiekszaO12}</li>
+    <li>Mniejsza o 1: ${mniejszaO12}</li>
+    <li>Zaokrąglona w dół: ${zaokraglonaWdol2}</li>
+    <li>Zaokrąglona w górę: ${zaokraglonaWgore2}</li>
+    <li>Zaokrąglona: ${zaokraglona2}</li>
+    <li>Pierwiastek: ${pierwiastek2}</li>
+    <li>Iloczyn z losową liczbą: ${iloczyn2}</li>
+    <li>Wartość absolutna: ${wartoscAbsolutna2}</li>
+    <li>Podwojona wartość: ${podwojonaWartosc2}</li>
+  </ul>
+  <p>Dla trzeciej liczby:</p>
+  <ul>
+    <li>Pierwiastek sumy: ${pierwiastekSumy}</li>
+    <li>Różnica kwadratów: ${roznicaKwadratow3}</li>
+    <li>Potęga trzecia: ${potegaTrzecia3}</li>
+    <li>Kwadrat liczby: ${kwadrat3}</li>
+    <li>Pierwiastek sześcienny: ${pierwiastekSzescienny3}</li>
+    <li>Średnia arytmetyczna: ${sredniaArytmetyczna3}</li>
+    <li>Wartość pomnożona przez 10: ${pomnozPrzez103}</li>
+    <li>Suma liczb całkowitych od 1 do podanej liczby: ${suma3}</li>
+  </ul>
+`;
