2d7f6a3cd44f6ec06b8848cc9ad7333eb9973f44
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-09-12 13:36:58 +0200

    update

---
+++ Funkcje/Funkcje/Program.cs
@@ -0,0 +1,46 @@
+﻿//Funkcje w C# są blokami kodu, które wykonują określone zadanie.
+//Mogą przyjmować parametry wejściowe, wykonywać operacje i zwracać wynik.
+//Funkcje są fundamentalnym elementem programowania, ponieważ pozwalają na modularność, ponowne użycie kodu oraz lepsze organizowanie logiki programu.
+
+//Funkcja składa się z typu jaki będzie zwracać, nazwy, oraz argumentów które funkcja przyjmuje
+
+// Funkcja dodająca dwie liczby całkowite
+int Add(int x, int y)
+{
+    // Dodaje x i y, a następnie zwraca wynik
+    return x + y;
+}
+
+// Funkcja odejmująca dwie liczby całkowite
+
+
+// Funkcja mnożąca dwie liczby całkowite
+
+
+// Funkcja dzieląca dwie liczby całkowite
+
+
+// Funkcja zwracająca resztę z dzielenia dwóch liczb całkowitych
+
+// Funkcja dodająca dwie liczby zmiennoprzecinkowe
+
+// Funkcja dodająca dwie liczby zmiennoprzecinkowe zwracająca liczbę całkowitą
+
+// Funkcja mnożąca dwie liczby zmiennoprzecinkowe zwracająca liczbę całkowitą
+
+// Funkcja mnożąca jedną liczbe zmiennoprzecinkową i jedną całkowitą zwracająca liczbę całkowitą
+
+// Funkcja mnożąca jedną liczbe zmiennoprzecinkową i jedną całkowitą zwracająca liczbę zmiennoprzecinkową
+
+
+// Funkcja dzieląca dwie liczby zmiennoprzecinkowe zwracająca liczbę całkowitą
+
+// Funkcja dzieląca jedną liczbe zmiennoprzecinkową i jedną całkowitą zwracająca liczbę całkowitą
+
+// Funkcja dzieląca jedną liczbe zmiennoprzecinkową i jedną całkowitą zwracająca liczbę zmiennoprzecinkową
+
+int a = 10;
+int b = 3;
+
+// Wywołanie funkcji i przypisanie wyniku do zmiennych
+int sum = Add(a, b); // Wywołanie funkcji dodającej dwie liczby
---
+++ Typy danych/Typy danych/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Xml.Linq;
+
+short liczbaUczniow = 30;
+double sredniaTemperatura = 23.5;
+string imie = "Anna";
+bool czyPogodaDobra = true;
+char pierwszaLitera = 'A';
+byte wiekDziecka = 8;
+long populacjaSwiata = 7800000000L;
+int[] oceny = { 5, 4, 3, 5, 6 };
+float[] przykladoweWartosci = { 1.1f, 2.2f, 3.3f };
+Dictionary<string, byte> liczbaKsiazek = new Dictionary<string, byte> { { "Matematyka", 5 }, { "Fizyka", 3 } };
+DateTime dataUrodzenia = new DateTime (1990, 5, 21);
+TimeOnly czasTrwania = new TimeOnly (2, 30, 0);
+int? wiek = null;
+dynamic status = czyPogodaDobra ? "Słonecznie" : "Pochmurno";
+decimal saldoKonta = 1023.75m;
+var punkt = (x: 10, y: 20);
+byte maksymalnaWartosc = 255;
+sbyte minimalnaWartosc = -128;
+ulong duzaLiczba = 10000000000000000000UL;
+dynamic zmienna = 10; zmienna = "Tekst";
+char[] samogloski = { 'a', 'e', 'i', 'o', 'u' };
+bool Dorosly = wiekDziecka >= 18;    
